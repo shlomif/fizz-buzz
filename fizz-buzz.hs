1dedde0231059fb2b41227c73b177a25fb23a991
Shlomi Fish <shlomif@shlomifish.org>
2018-01-20 18:29:25 +0200

    Add haskell.

---
+++ fizz-buzz.hs
@@ -0,0 +1,11 @@
+-- Taken from https://wiki.haskell.org/Fizzbuzz under a simple
+-- permissive licence - thanks!
+fizz :: Int -> String
+fizz n | n `mod` 15 == 0  = "FizzBuzz"
+       | n `mod` 3  == 0  = "Fizz"
+       | n `mod` 5  == 0  = "Buzz"
+       | otherwise        = show n
+
+main :: IO()
+main = mapM_ putStrLn $ map fizz [1..100]
+
