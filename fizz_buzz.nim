ae4ecea98afa97aec69ade5131fd0d47c5af797d
Shlomi Fish <shlomif@shlomifish.org>
2020-09-10 17:42:30 +0300

    convert to the $-stringification op (thanks to Prestige)

--- fizz_buzz.nim
+++ fizz_buzz.nim
@@ -41,9 +41,7 @@ OTHER DEALINGS IN THE SOFTWARE.
 
 ]#
 
-import strformat
-
 for i in countup(1, 100):
     let div3 = (i mod 3 == 0)
     let div5 = (i mod 5 == 0)
-    echo(if div3: (if div5: "FizzBuzz" else: "Fizz") else: (if div5: "Buzz" else: fmt"{i}"))
+    echo(if div3: (if div5: "FizzBuzz" else: "Fizz") else: (if div5: "Buzz" else: $i))
